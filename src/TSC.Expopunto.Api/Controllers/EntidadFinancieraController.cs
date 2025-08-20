10b6895e07f6259023d87bc1c7e0c8fd430126f9
Jairo <txcwh1199@gmail.com>
2025-08-20 12:08:30 -0500

    entidad controlador creada

---
+++ src/TSC.Expopunto.Api/Controllers/EntidadFinancieraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSC.Expopunto.Api.Controllers
+{
+    public class EntidadFinancieraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
