a7cdeb00ca35ec9be4b47a2084a102c81176611f
Delia Garcia <deliadecotrina@gmail.com>
2025-08-21 11:13:10 -0500

    mi primer cambio

---
+++ src/TSC.Expopunto.Api/Controllers/TipoDocumentoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TSC.Expopunto.Api.Controllers
+{
+    [ApiController]
+    public class TipoDocumentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
