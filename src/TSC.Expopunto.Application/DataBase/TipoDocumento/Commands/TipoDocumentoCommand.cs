154d9323bef5772ba6c70e37f73d0a4b8559d9dc
Jhohan Moran <39931652+JhohanMoran@users.noreply.github.com>
2025-08-22 13:42:30 -0500

    ultimo commit para iniciar primer merge de cambios

---
+++ src/TSC.Expopunto.Application/DataBase/TipoDocumento/Commands/TipoDocumentoCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace TSC.Expopunto.Application.DataBase.TipoDocumento.Commands
+{
+    public class TipoDocumentoCommand : ITipoDocumentoCommand
+    {
+        private readonly IDapperCommandService _dapperService;
+
+        public TipoDocumentoCommand(IDapperCommandService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+
+        public async Task<TipoDocumentoModel> ProcesarAsync(TipoDocumentoModel model)
+        {
+            var response = await _dapperService.ExecuteScalarAsync("uspSetTipoDocumento",
+                new
+                {
+                    p_opcion = model.opcion,
+
+                    p_id = model.id,
+                    p_codigo = model.codigo,
+                    p_descripcion = model.descripcion,
+
+                    p_idUsuario = model.idUsuario,
+                    p_activo = model.activo
+                });
+
+            if (response > 1)
+            {
+                model.id = response;
+            }
+
+            return model;
+
+        }
+    }
+}
