2379be3d4116c5f937049bc7fb5fa04c30b66f80
Jhohan Moran <39931652+JhohanMoran@users.noreply.github.com>
2025-08-22 14:39:06 -0500

    modulo terminado

--- src/TSC.Expopunto.Application/DataBase/Usuario/Queries/Models/UsuariosTodos.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TSC.Expopunto.Application.DataBase.Usuario.Queries.Models
-{
-    public class UsuariosTodos
-    {
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public string usuario { get; set; }
-    }
-}
