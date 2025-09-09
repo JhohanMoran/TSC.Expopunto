acd3badf8dcb4c5c56fda36e75dab0ccaae22194
Delia Garcia <deliadecotrina@gmail.com>
2025-09-09 16:10:09 -0500

    READ de la tabla UnidadesMedida finalizado

--- src/TSC.Expopunto.Application/DependencyInjectionService.cs
+++ src/TSC.Expopunto.Application/DependencyInjectionService.cs
@@ -13,6 +13,7 @@ using TSC.Expopunto.Application.DataBase.PerfilMenu.Commands;
 using TSC.Expopunto.Application.DataBase.PerfilMenu.Queries;
 using TSC.Expopunto.Application.DataBase.Sede.Commands;
 using TSC.Expopunto.Application.DataBase.Sede.Queries;
+using TSC.Expopunto.Application.DataBase.UnidadMedida.Queries;
 using TSC.Expopunto.Application.DataBase.TipoComprobante.Queries;
 using TSC.Expopunto.Application.DataBase.TipoDocumento.Commands;
 using TSC.Expopunto.Application.DataBase.TipoDocumento.Queries;
@@ -78,6 +79,8 @@ namespace TSC.Expopunto.Application
             services.AddTransient<IFormaPagoQuery,  FormaPagoQuery>();
 
             services.AddTransient<IMedioPagoQuery, MedioPagoQuery>();
+
+            services.AddTransient<IUnidadMedidaQuery, UnidadMedidaQuery>();
             #region Validators
             services.AddScoped<IValidator<PerfilModel>, CrearPerfilValidator>();
             services.AddScoped<IValidator<PerfilMenuModel>, PerfilMenuValidator>();
