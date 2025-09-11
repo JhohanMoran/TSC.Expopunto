3026b084eda595f6762f5d88d011fe66685e09f6
Jean Pierre <k300015@gmail.com>
2025-09-10 19:29:52 -0500

    se agrega usuarios sede

---
+++ src/TSC.Expopunto.Application/DataBase/UsuariosSede/Queries/Models/UsuariosSedeParam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSC.Expopunto.Application.DataBase.UsuariosSede.Queries.Models
+{
+    public class UsuariosSedeParam
+    {
+        public int? IdUsuario { get; set; }
+        public int? IdSede { get; set; }
+    }
+}
