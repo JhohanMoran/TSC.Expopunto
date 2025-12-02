d0fc4da576b69ab6db4d7526a11f651ac589e9e8
Jean Pierre <k300015@gmail.com>
2025-12-02 18:37:56 -0500

    se agrega generacion de txt por venta

---
+++ src/TSC.Expopunto.Api/Controllers/EmisionComprobanteSunatController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TSC.Expopunto.Application.DataBase.EmisionComprobanteSunat.Queries.ObtenerComprobanteSunat;
+using TSC.Expopunto.Application.DataBase.EmisionComprobanteSunat.Queries.ObtenerComprobanteSunat.Params;
+using TSC.Expopunto.Application.Exceptions;
+using TSC.Expopunto.Application.Features;
+
+namespace TSC.Expopunto.Api.Controllers
+{
+    [Route("api/v1/comprobante-sunat")]
+    [ApiController]
+    [TypeFilter(typeof(ExceptionManager))]
+    public class EmisionComprobanteSunatController : Controller
+    {
+        private readonly IMediator _mediator;
+        public EmisionComprobanteSunatController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost("emitir")]
+        public async Task<IActionResult> EmitirComprobanteSunat(
+            [FromBody] ObtenerComprobanteSunatParams parametros
+        )
+        {
+            await _mediator.Send(new ObtenerComprobanteSunatQuery(parametros));
+            return StatusCode(
+                StatusCodes.Status204NoContent,
+                ResponseApiService.Response(StatusCodes.Status204NoContent, message:"Exitoso")
+            );
+        }
+    }
+}
---
+++ src/TSC.Expopunto.Application/Interfaces/Repositories/EmisionComprobanteSunat/IEmisionComprobanteSunatRepository.cs
@@ -0,0 +1,10 @@
+﻿using TSC.Expopunto.Application.DataBase.EmisionComprobanteSunat.Queries.ObtenerComprobanteSunat.Params;
+using TSC.Expopunto.Domain.Entities.EmisionComprobanteSunat;
+
+namespace TSC.Expopunto.Application.Interfaces.Repositories.EmisionComprobanteSunat
+{
+    public interface IEmisionComprobanteSunatRepository
+    {
+        Task<List<ComprobanteSunatEntity>> ObtenerComprobanteSunatAsync(ObtenerComprobanteSunatParams parametro);
+    }
+}
